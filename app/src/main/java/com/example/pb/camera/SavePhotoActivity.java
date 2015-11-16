bbc9509de76a125263c996af72a54b24333709e8
SergeyDimitrov <Strike.64140@mail.ru>
2015-11-16 11:48:02 +0400

    Added proper releasing of resources

--- app/src/main/java/com/example/pb/camera/SavePhotoActivity.java
+++ app/src/main/java/com/example/pb/camera/SavePhotoActivity.java
@@ -26,6 +26,8 @@ public class SavePhotoActivity extends Activity {
     private Button savePhotoButton;
     private AlertDialog saveDialog;
 
+    private Bitmap image;
+
     private SharedPreferences settings;
     private static final String APP_PREFERENCES = "preferences";
     private static final String APP_PREFERENCES_FILENAME = "filename";
@@ -72,7 +74,8 @@ public class SavePhotoActivity extends Activity {
         File photo = new File(getFilesDir(), getResources().getString(R.string.temp_filename));
 
         if (photo.exists()) {
-            photoView.setImageBitmap(decodeSampledBitmap(100, 100));
+            image = BitmapFactory.decodeFile(photo.getAbsolutePath());
+            photoView.setImageBitmap(image);
         } else {
             Toast.makeText(this, R.string.no_photo_error, Toast.LENGTH_SHORT).show();
             finish();
@@ -96,6 +99,11 @@ public class SavePhotoActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        photoView.setImageBitmap(null);
+        if (image != null) image.recycle();
+        image = null;
+
         unregisterReceiver(receiver);
         SharedPreferences.Editor editor = settings.edit();
         if (isChangingConfigurations()) {
@@ -108,6 +116,8 @@ public class SavePhotoActivity extends Activity {
             editor.remove(APP_PREFERENCES_FILENAME);
         }
         editor.apply();
+        if (saveDialog != null) saveDialog.cancel();
+        saveDialog = null;
     }
 
     private Dialog createDialog() {
@@ -133,38 +143,6 @@ public class SavePhotoActivity extends Activity {
                     }
                 });
         return saveDialog = builder.create();
-    }
-
-    private Bitmap decodeSampledBitmap(int reqWidth, int reqHeight) {
-        File file = new File(getFilesDir(), getResources().getString(R.string.temp_filename));
-
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-
-
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-    }
-
-    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
     }
 
 }
